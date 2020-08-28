afc945d35230b624470e32cf906ab94606861d9a
gfilewicz <Grzegorz.Filewicz@coi.gov.pl>
2020-08-28 14:01:28 +0200

    combat logic

--- combat/src/main/java/com/filewicz/combat/controller/CombatRestControllerImpl.java
+++ combat/src/main/java/com/filewicz/combat/controller/CombatRestControllerImpl.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -19,15 +19,36 @@ public class CombatRestControllerImpl {
 
     private final CombatService combatService;
     private final RestTemplate restTemplate;
-
+    private int statusCodeValue = 200;
 
     @GetMapping("/fight")
     //http://localhost:8088/api/combat?heroes1=Benq&heroes2=Fendt
-   public String fight(@RequestParam String heroes1,@RequestParam String heroes2){
+    public String fight(@RequestParam String heroes1, @RequestParam String heroes2) {
 
-        Heroes hero1 = restTemplate.getForObject(HEROES_URL + heroes1, Heroes.class);
-        Heroes hero2 = restTemplate.getForObject(HEROES_URL + heroes2, Heroes.class);
+        Heroes hero1 = null;
+        Heroes hero2 = null;
 
-        return combatService.fight(hero1,hero2);
-   }
+        hero1 = heroesExistanceValidate(heroes1);
+        if (statusCodeValue == 404) {
+            statusCodeValue = 200;
+            return "Heroes with name " + heroes1 + " not found";
+        } else if (statusCodeValue == 200) {
+            hero2 = heroesExistanceValidate(heroes2);
+            if (statusCodeValue == 404) {
+                statusCodeValue = 200;
+                return "Heroes with name " + heroes2 + " not found";
+            }
+        }
+        return combatService.fight(hero1, hero2);
+    }
+
+    private Heroes heroesExistanceValidate(String heroesName) {
+        Heroes heroes = null;
+        try {
+            heroes = restTemplate.getForObject(HEROES_URL + heroesName, Heroes.class);
+        } catch (HttpStatusCodeException e) {
+            statusCodeValue = e.getStatusCode().value();
+        }
+        return heroes;
+    }
 }
