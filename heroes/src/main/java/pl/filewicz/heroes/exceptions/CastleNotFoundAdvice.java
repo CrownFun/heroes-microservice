7f39d39fb20bede1023e7d27f4719e39782c6aeb
gfilewicz <Grzegorz.Filewicz@coi.gov.pl>
2020-08-30 15:00:44 +0200

    obsluga wyjatkow towrzenia herosa

---
+++ heroes/src/main/java/pl/filewicz/heroes/exceptions/CastleNotFoundAdvice.java
@@ -0,0 +1,16 @@
+package pl.filewicz.heroes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CastleNotFoundAdvice {
+
+    @ExceptionHandler(CastleNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String castleNotFoundHandler(CastleNotFoundException e) {
+        return e.getMessage();
+    }
+}
